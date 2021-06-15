e9b9da26525abe5ce5a899c21522c72030945ae6
AgostinaM <agosmorellato@gmail.com>
2021-06-15 10:07:25 -0300

    Se realizo el ejercicio de dibujar una imagen

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -197,3 +197,17 @@ function cuadricular(){
     ctx.closePath();
 
 }
+
+function dibujarImg (posX, posY){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    console.log(posX, posY);
+    var img =new Image();
+    img.src ="images/auto.png";
+    canvas.width=canvas.width;
+
+    img.onload= function (){
+        ctx.drawImage(img, posX, posY);
+    }
+
+}
